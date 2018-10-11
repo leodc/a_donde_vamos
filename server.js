33a6d3c1c61c810efeac3e2a8acbb98958223b17
Leonel Castañeda <leodcastaneda@gmail.com>
2018-10-11 13:41:19 -0500

    add request signature validation

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
 var express = require("express");
 var bodyParser = require("body-parser");
+var crypto = require("crypto");
 
 // creates express http server
 var app = express();
 app.set("port", process.env.PORT || 80);
-app.use(bodyParser.json());
+app.use(bodyParser.json({ verify: verifyRequestSignature }));
 
 // Routers
 var webhookRouter = require("./routers/webhook");
@@ -12,3 +13,23 @@ app.use("/webhook", webhookRouter);
 
 // Sets server port and logs message on success
 app.listen( app.get("port"), () => console.log("Bot is running on port " + app.get("port")));
+
+
+// request validation
+function verifyRequestSignature(req, res, buf) {
+  var signature = req.headers["x-hub-signature"];
+
+  if (!signature) {
+    console.error("Couldn't validate the signature");
+  } else {
+    // valid signature
+    var elements = signature.split("=");
+    //var method = elements[0];
+    var signatureHash = elements[1];
+
+    var expectedHash = crypto.createHmac("sha1", process.env.A_DONDE_APP_SECRET ).update(buf).digest("hex");
+    if (signatureHash != expectedHash) {
+      console.error("Bad signature");
+    }
+  }
+}
